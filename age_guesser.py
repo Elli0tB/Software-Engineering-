09695a764d10648744b5fcdcdc058d2104873de2
Elliot Bangerter <elliot.bangerter@gmail.com>
2025-08-27 10:26:08 -0600

    Fixed the Infinit loop issue on line 6-9. Age still not returning correctly

--- age_guesser.py
+++ age_guesser.py
@@ -3,21 +3,21 @@ import random
 def guessAge():
     guess = random.randint(15,30)
     ansr = input(f"are you {guess} years old? (y/n):")
-    if ansr != 'n' or 'y':
-        print("that is invailid input, lets try again")
-        guessAge()
-    while ansr == 'n':
+   # if ansr != 'n' or 'y':
+   #     print("that is invailid input, lets try again")
+   #     guessAge()
+    if ansr == 'n':
         print("RATS!")
         guessAge()
     if ansr == 'y':
-        return ansr
+        return guess
     
     
 def start():
     print("Hello I am going to guess you age")
     usrName = input("first what is your name:")
     guess = guessAge()
-    print( f"{usrName}is {guess} years old.")
+    print( f"{usrName} is {guess} years old.")
     quit
 
 start()
